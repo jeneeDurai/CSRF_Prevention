5d0e74a0f7adfb9683619dba79dd7854695e30fe
JeneepanRajadurai <jeneepan.raja@gmail.com>
2018-08-28 07:07:38 +0530

    Added Styles

--- CSRF_Prevention/index.php
+++ CSRF_Prevention/index.php
@@ -6,14 +6,64 @@
     $_SESSION['csrf'] = $rand;
 ?>
 
+    <html lang="en">
 
-<form method="post" action  = "save_user.php">
+    <head>
+        <title>CSRF</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-username : <input type = "text" name = "uname" /></br>
-password : <input type = "text" name = "pwd"/> </br>
+        <link rel="stylesheet" type="text/css" href="css/util.css">
+        <link rel="stylesheet" type="text/css" href="css/main.css">
+    </head>
 
-<input type="hidden" name ="_token" value="<?php echo $rand?>"?/>
+    <body>
 
-<input type = "submit" name ="submit" value="Add new user"/>
+        <div class="limiter">
+            <div class="container-login100">
+                <div class="wrap-login100">
+                    <div class="login100-form-title" style="background-image: url(images/bg-01.jpg);">
+                        <span class="login100-form-title-1">
+						CSRF Prevention
+					</span>
+                    </div>
 
-</form>
+                    <form class="login100-form validate-form" method="post" action="save_user.php">
+                        <div class="wrap-input100 validate-input m-b-26" data-validate="Username is required">
+                            <span class="label-input100">Username</span>
+                            <input class="input100" type="text" name="uname" placeholder="Enter username">
+                            <span class="focus-input100"></span>
+                        </div>
+
+                        <div class="wrap-input100 validate-input m-b-18" data-validate="Password is required">
+                            <span class="label-input100">Password</span>
+                            <input class="input100" type="password" name="pwd" placeholder="Enter password">
+                            <span class="focus-input100"></span>
+                        </div>
+
+                        <div class="flex-sb-m w-full p-b-30">
+                            <div class="contact100-form-checkbox">
+                                <input class="input-checkbox100" id="ckb1" type="checkbox" name="remember-me">
+                                <label class="label-checkbox100" for="ckb1">
+								Remember me
+							</label>
+                            </div>
+
+                            <div>
+                                <a href="#" class="txt1">
+								Register
+							</a>
+                            </div>
+                        </div>
+                        <input type="hidden" name ="_token" value="<?php echo $rand?>"?/>
+                        <div class="container-login100-form-btn">
+                            <input type="submit" class="login100-form-btn" value="Login">
+                            </button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </body>
+
+    </html>
